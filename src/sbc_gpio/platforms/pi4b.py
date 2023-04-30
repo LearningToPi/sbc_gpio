84dfef84bfc8608d0f19cd981566cbb683b946dc
Thomas Dunteman <104242297+LearningToPi@users.noreply.github.com>
2023-04-29 22:49:01 -0700

    updated for pi4B

--- src/sbc_gpio/platforms/pi4b.py
+++ src/sbc_gpio/platforms/pi4b.py
@@ -1,22 +1,19 @@
+from collections import namedtuple
+from sbc_gpio import PLATFORM_INFO
+
+# select the gpio library for the platform
+from sbc_gpio.gpio_libs.gpiod import GpioIn, GpioOut
+
 MODEL_IDENTIFIER = [
     {
         'type': 'file', 'file': '/sys/firmware/devicetree/base/model', 'contents': 'Raspberry Pi 4 Model B'
     }
 ]
 
-MODEL_DESCRIPTION = 'Raspberry Pi 4 Model B'
-MODEL_SHORT = 'Pi4B'
+SERIAL_NUMBER = '/sys/firmware/devicetree/base/serial-number'
 
-LED_CHIP = 0
-LED_GPIO = 24 # Rock5B GPIO3_C1, GPIO 113 - PI4 GPIO0_23
-BUTTON_CHIP = 0
-BUTTON_GPIO = 23 # Rock5B GPIO3_B6, GPIO 110 - PI4 GPIO0_25
-BMX280_SPI_BUS = 1
-BMX280_SOFT_CS_PIN = 13
-BMX280_SOFT_CS_CHIP = 0
-DHT11_SPI_BUS = 0
-DHT11_SOFT_CS_PIN = 26
-DHT11_SOFT_CS_CHIP = 0
-I2C_DISPLAY_PORT = 1
-SER_1_DEV = '/dev/ttyS0'
-SER_2_DEV = '/dev/ttyUSB0'
+PLATFORM_LOCAL = namedtuple('PLATFORM_LOCAL', ('serial_path'))
+PLATFORM_SPECIFIC = PLATFORM_INFO(model='Pi4B',
+              description='Raspberry Pi 4 Model B',
+              gpio_valid_values=[0, 1, 2, 3, 4, 5, 6, 7 , 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27],
+              local=PLATFORM_LOCAL(serial_path='/sys/firmware/devicetree/base/serial-number'))
--- src/sbc_gpio/tests/devtester_test.py
+++ src/sbc_gpio/tests/devtester_test.py
@@ -10,6 +10,7 @@ from sbc_gpio.device_tests.button_gpiod import DevTest_Button
 from sbc_gpio.device_tests.dht_spi import DevTest_DHT
 from sbc_gpio.device_tests.bmx_spi import DevTest_BMX
 from sbc_gpio.device_tests.i2c_display import DevTest_I2CDisp
+from sbc_gpio.device_tests.uart import DevTest_UART
 
 logger = create_logger(INFO, name='tester')
 
@@ -22,6 +23,19 @@ gpio_tests = {
         'bmx': '3B5',
         'bmx_spi': 1,
         'i2c': 7
+    },
+    'Pi4B': {
+        "led": 17,
+        "btn": 27,
+        "dht": 5,
+        "dht22": False,
+        "dht_spi": 0,
+        "bmx": 16,
+        "bmx_spi": 1,
+        "i2c": 1,
+        "ir": True,
+        "uart_dev": "ttyS0",
+        "usb_dev": "ttyUSB0"
     }
 }
 
@@ -39,7 +53,7 @@ class devtesterTest(unittest.TestCase):
         logger.info(f'{dev_tester.test_results}')
         self.assertTrue(dev_tester.test_results.passed)
         logger.info('----------------- Test 10 seconds blocking')
-        dev_tester.start(run_secs=10, wait=True)
+        dev_tester.start(run_secs=30, wait=True)
         logger.info(f'{dev_tester.test_results}')
         self.assertTrue(dev_tester.test_results.passed)
 
@@ -73,7 +87,7 @@ class devtesterTest(unittest.TestCase):
     def test_4_dht(self):
         logger.info('===================================== %s', self._testMethodName)
         pin = platform.convert_gpio_tuple(test_vars.get('dht'))
-        dht_tester = DevTest_DHT(int(test_vars.get('dht_spi', 0)), pin, log_level=INFO, dht22=True)
+        dht_tester = DevTest_DHT(int(test_vars.get('dht_spi', 0)), pin, log_level=INFO, dht22=False)
         logger.info('----------------- Test 10 iterations blocking')
         dht_tester.start(iterations=10, wait=True)
         logger.info(f'{dht_tester.test_results}')
@@ -108,6 +122,22 @@ class devtesterTest(unittest.TestCase):
         logger.info(f'{i2c_tester.test_results}')
         self.assertTrue(i2c_tester.test_results.passed)
 
+    def test_7_uart(self):
+        logger.info('===================================== %s', self._testMethodName)
+        uart_tester = DevTest_UART(test_vars.get('uart_dev'), test_vars.get('usb_dev'), log_level=DEBUG)
+        logger.info('----------------- Test 10 iterations blocking')
+        uart_tester.start(iterations=10, wait=True)
+        logger.info(f'{uart_tester.test_results}')
+        for detail in uart_tester.test_results.details:
+            logger.info(f"    {detail}")
+        self.assertTrue(uart_tester.test_results.passed)
+        logger.info('----------------- Test 10 seconds blocking')
+        uart_tester.start(run_secs=120, wait=True)
+        logger.info(f'{uart_tester.test_results}')
+        for detail in uart_tester.test_results.details:
+            logger.info(f"    {detail}")
+        self.assertTrue(uart_tester.test_results.passed)
+
     def test_7_threading(self):
         logger.info('===================================== %s', self._testMethodName)
         bmx_pin = platform.convert_gpio_tuple(test_vars.get('bmx'))
@@ -119,7 +149,8 @@ class devtesterTest(unittest.TestCase):
         led_pin = platform.get_gpio_out(test_vars.get('led'))
         led_tester = DevTest_LED(led_pin, log_level=INFO)
         ir_tester = DevTest_IR(log_level=INFO)
-        i2c_tester = DevTest_I2CDisp(7, log_level=INFO)
+        uart_tester = DevTest_UART(test_vars.get('uart_dev'), test_vars.get('usb_dev'), log_level=INFO)
+        i2c_tester = DevTest_I2CDisp(test_vars.get('i2c'), log_level=INFO)
         # verify all tests are ok to run
         self.assertTrue(bmx_tester.is_test_ok and dht_tester.is_test_ok and btn_tester.is_test_ok and led_tester.is_test_ok and ir_tester.is_test_ok and i2c_tester.is_test_ok)
         # start all the threads for 30 seconds
@@ -130,6 +161,7 @@ class devtesterTest(unittest.TestCase):
         led_tester.start(run_secs=run_secs)
         ir_tester.start(run_secs=run_secs)
         i2c_tester.start(run_secs=run_secs)
+        uart_tester.start(run_secs=run_secs)
         stop_time = time() + run_secs
         while time() < stop_time:
             logger.info(f'Tasks Running:  BMX: {bmx_tester.is_running}, DHT: {dht_tester.is_running}, BTN: {btn_tester.is_running}, LED: {led_tester.is_running}, IR: {ir_tester.is_running}, I2C: {i2c_tester.is_running}')
@@ -138,10 +170,13 @@ class devtesterTest(unittest.TestCase):
             logger.info(f'Tasks Running:  BMX: {bmx_tester.is_running}, DHT: {dht_tester.is_running}, BTN: {btn_tester.is_running}, LED: {led_tester.is_running}, IR: {ir_tester.is_running}, I2C: {i2c_tester.is_running}')
             sleep(1)
         self.assertFalse(bmx_tester.is_running and dht_tester.is_running and btn_tester.is_running and led_tester.is_running and ir_tester.is_running and i2c_tester.is_running)
-        self.assertTrue(bmx_tester.test_results.passed and dht_tester.test_results.passed and btn_tester.test_results.passed and led_tester.test_results.passed and ir_tester.test_results.passed and i2c_tester.test_results.passed)
-        logger.info(f"BMX results: {bmx_tester.test_results}")
-        logger.info(f"DHT results: {dht_tester.test_results}")
-        logger.info(f"BTN results: {btn_tester.test_results}")
-        logger.info(f"LED results: {led_tester.test_results}")
-        logger.info(f"IR  results: {ir_tester.test_results}")
-        logger.info(f"I2C results: {i2c_tester.test_results}")
+        logger.info(f"BMX results:  {bmx_tester.test_results}")
+        logger.info(f"DHT results:  {dht_tester.test_results}")
+        logger.info(f"BTN results:  {btn_tester.test_results}")
+        logger.info(f"LED results:  {led_tester.test_results}")
+        logger.info(f"IR  results:  {ir_tester.test_results}")
+        logger.info(f"I2C results:  {i2c_tester.test_results}")
+        logger.info(f"UART Results: {uart_tester.test_results}")
+        for detail in uart_tester.test_results.details:
+            logger.info(f"    {detail}")
+        self.assertTrue(bmx_tester.test_results.passed and dht_tester.test_results.passed and btn_tester.test_results.passed and led_tester.test_results.passed and ir_tester.test_results.passed and i2c_tester.test_results.passed and uart_tester.test_results.passed)
