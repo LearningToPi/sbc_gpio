a7ee6c832da53b9c6e234fb77750811c63174fb1
Thomas Dunteman <104242297+LearningToPi@users.noreply.github.com>
2023-06-28 13:16:33 -0700

    fix pi to try RPi.GPIO then fallback to gpiod

--- src/sbc_gpio/platforms/rpi.py
+++ src/sbc_gpio/platforms/rpi.py
@@ -8,7 +8,12 @@ the future to include other platforms.
 from sbc_gpio import PLATFORM_INFO
 
 # select the gpio library for the platform
-from sbc_gpio.gpio_libs.rpi_gpio import GpioIn, GpioOut #pylint: disable=W0611
+try:
+    # try using The RPi.GPIO library
+    from sbc_gpio.gpio_libs.rpi_gpio import GpioIn, GpioOut #pylint: disable=W0611
+except ImportError:
+    # fallback to gpiod
+    from sbc_gpio.gpio_libs.gpiod import GpioIn, GpioOut #pylint: disable=W0611,C0411
 
 PLATFORM_SPECIFIC = PLATFORM_INFO(
               gpio_valid_values=[0, 1, 2, 3, 4, 5, 6, 7 , 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27],
